d75f52c9b8f985599b67b3828d857dfcc44a74fa
Andrii Klimkovskyi <andrew.klimkovskyi@gmail.com>
2024-09-26 23:58:06 +0300

    created delete, post, patch routes

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import {
+  createContactController,
+  deleteContactByIdController,
   getContactByIdController,
   getContactsController,
+  patchContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -12,6 +15,18 @@ contactsRouter.get('/contacts', ctrlWrapper(getContactsController));
 contactsRouter.get(
   '/contacts/:contactId',
   ctrlWrapper(getContactByIdController),
+);
+
+contactsRouter.post('/contacts', ctrlWrapper(createContactController));
+
+contactsRouter.patch(
+  '/contacts/:contactId',
+  ctrlWrapper(patchContactController),
+);
+
+contactsRouter.delete(
+  '/contacts/:contactId',
+  ctrlWrapper(deleteContactByIdController),
 );
 
 export default contactsRouter;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,7 +4,36 @@ export const getAllContacts = async () => {
   const contacts = await contactModel.find();
   return contacts;
 };
-export const getContactById = async (studentId) => {
-  const contact = await contactModel.findById(studentId);
+
+export const getContactById = async (contactId) => {
+  const contact = await contactModel.findById(contactId);
+  return contact;
+};
+
+export const createContact = async (payload) => {
+  const contact = await contactModel.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactId, payload) => {
+  const rawResult = await contactModel.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+  };
+};
+
+export const deleteContactById = async (contactId) => {
+  const contact = await contactModel.findOneAndDelete(contactId);
+
   return contact;
 };
